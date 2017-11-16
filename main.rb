280dea05ce439ea621118e7693b26ba789d6fb30
Andrew Brown <drusepth@gmail.com>
2017-11-16 19:39:09 +0100

    named speaker extraction

--- main.rb
+++ main.rb
@@ -1,9 +1,14 @@
 #!/usr/bin/ruby
 require 'pry'
 
-START_QUOTE = '"'
-END_QUOTE   = '"'
-DIALOGUE_REGEX = /#{START_QUOTE}[^#{START_QUOTE}]+#{END_QUOTE}/
+START_QUOTE         = '"'
+END_QUOTE           = '"'
+DIALOGUE_REGEX      = /#{START_QUOTE}[^#{START_QUOTE}]+#{END_QUOTE}/
+SPEAKER_NAME_REGEX  = /[A-Z][\w\.-]*\s+[A-Z][\w-]*+/
+SPEAKER_ANNOTATIONS = [
+  /said (#{SPEAKER_NAME_REGEX})/,
+  /(#{SPEAKER_NAME_REGEX}) said/
+]
 
 unless book_path = ARGV.shift
   puts "Usage: $0 <path/to/book.txt>"
@@ -14,6 +19,16 @@ def dialogue_in_text text
   text.scan DIALOGUE_REGEX
 end
 
+def speaker_in_text text
+  SPEAKER_ANNOTATIONS.each do |regex|
+    match = text.scan regex
+    return match.flatten[0] if match.any?
+  end
+
+  return nil
+end
+
+binding.pry
 
 File.open(book_path, "r") do |f|
   f.each_line do |line|
@@ -23,6 +38,9 @@ File.open(book_path, "r") do |f|
     dialogue = dialogue_in_text(line)
     next unless dialogue.any?
 
-    puts "Dialogue found: #{dialogue}"
+    # Find any potential speaker
+    speaker = speaker_in_text(line)
+
+    puts "<#{speaker}> #{dialogue}"
   end
 end
